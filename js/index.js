a0890ee753cbab9b77ba7d0d2ce670bba29611d3
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-12-08 12:00:35 +0800

    replace api

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-const WEATHER_BASE_URL = 'http://47.106.250.72:8089/';
+const WEATHER_BASE_URL = 'https://my-weather12138.herokuapp.com/';
+const provinceElement = document.querySelector("#province");
+const cityElement = document.querySelector("#city");
+
 const createWeatherUrl = (type = "forecast_24h", province, city) => `${WEATHER_BASE_URL}?source=xw&weather_type=${type}&province=${province}&city=${city}`;
 
 function createElement(innerHTML,tagName="span",style="margin: 0 1rem 0 1rem;") {
@@ -24,8 +27,8 @@ class WeatherItem {
     const element = document.createElement('li');
     element.appendChild(createElement(`日间天气：${this.dayWeather}`));
     element.appendChild(createElement(`夜间天气：${this.nightWeather}`));
-    element.appendChild(createElement(`最低温度：${this.minDegree}`));
-    element.appendChild(createElement(`最高温度：${this.maxDegree}`));
+    element.appendChild(createElement(`最低温度：${this.minDegree}℃`));
+    element.appendChild(createElement(`最高温度：${this.maxDegree}℃`));
     element.appendChild(createElement(`时间：${this.time}`));
     if (this.classNames) {
       element.classList.add(...this.classNames.split(" "));
@@ -38,18 +41,19 @@ class WeatherItem {
 }
 
 async function fetchWeatherDetail() {
-  const provinceText = document.querySelector("#province").value;
-  const cityText = document.querySelector("#city").value;
   const newDisplayContainer = document.createElement('ol');
   newDisplayContainer.id = "display-container";
+  const inputContainer = document.querySelector("#input-container");
 
-  const response = await fetch(createWeatherUrl("forecast_24h", provinceText, cityText));
+  inputContainer.classList.add("flash-infinite");
+  const response = await fetch(createWeatherUrl("forecast_24h", provinceElement.value, cityElement.value));
   const data = await response.json();
+  inputContainer.classList.remove("flash-infinite");
+
   if (!data.data || !data.data.forecast_24h || !data.data.forecast_24h["0"]) {
-    const inputContainer = document.querySelector("#input-container");
-    inputContainer.classList.remove("shake");
+    inputContainer.classList.remove("my-shake");
     setTimeout(() => {
-      inputContainer.classList.add("shake");
+      inputContainer.classList.add("my-shake");
     }, 300);
     return;
   }
